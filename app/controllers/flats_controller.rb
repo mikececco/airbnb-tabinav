9b174b4b0bbfc376d1ed89743b45aa7a954486ec
Ronnie Hernaman <ronnieh7@hotmail.com>
2023-02-27 16:30:02 +0100

    created flat MVC

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,17 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+end
---
+++ app/models/flat.rb
@@ -0,0 +1,5 @@
+class Flat < ApplicationRecord
+  # belongs_to :user
+  validates :address, presence: true
+  validates :price, presence: true
+end
