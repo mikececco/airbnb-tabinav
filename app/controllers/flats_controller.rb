8459a36a15a16cc4e618c143fa6d3f1a9fa166ba
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-03-01 14:02:29 +0100

    map with popup

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -13,7 +13,8 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {flat: flat})
       }
     end
   end
