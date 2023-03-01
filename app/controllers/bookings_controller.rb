aaca5b03db656749489bb804d8b6781fb94ea9c8
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-03-01 15:41:01 +0100

    booking confirmation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,10 +3,11 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
-  # # show: will display the details of a specific booking, such as the price and location
-  # def show
-  #   @booking = Booking.find(params[:id])
-  # end
+  # show: will display the details of a specific booking, such as the price and location
+  def show
+    @booking = Booking.find(params[:id])
+    @flat = @booking.flat
+  end
 
   # new: will display a form to create a new booking
   def new
@@ -21,7 +22,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
 
     if @booking.save
-      redirect_to flats_path
+      redirect_to booking_path(@booking)
     else
       render :new, status: :unprocessable_entity
     end
