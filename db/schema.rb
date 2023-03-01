74d73e5e1edd1592957f1b9fea3c09d33ec6c985
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-03-01 12:17:18 +0100

    basic map feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_154722) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_094550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_154722) do
     t.string "flat_location"
     t.bigint "user_id"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,24 +21,53 @@ admin = User.create!(email: "admin@tabinav.com", password: "111111", first_name:
 
 puts "Accounts for Jeroen, Mike, Ronnie, Eirene were created! \nEmail: jeroen@tabinav.com | mike@tabinav.com | ronnie@tabinav.com | eirene@tabinav.com \nPassword: 111111"
 puts "---\n"
-puts "Creating 50 flats..."
+puts "Creating 30 flats in NL..."
 
-50.times do
-  address = Faker::Address.street_address
-  city = ["Amsterdam", "Rotterdam", "Utrecht", "The Hague"].sample
-  country = "NL"
-  file = URI.open("https://source.unsplash.com/random/600x400/?#{city}")
-  price = rand(39..109)
-  flat = Flat.new(address: address, city: city, flat_location: country, price: price, user: admin)
-  flat.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-  flat.save!
+nl_addresses = {
+  "Amsterdam" => ["537 Pinasstraat", "572 Kloekhorststraat", "IJsbaanpad 9", "18, Erik de Roodestraat", "5C, Vincent van Gogh Street", "4, Koningin Wilhelminaplein", "371, Prinseneiland", "23, Pennyhof", "29, M.C. Addicksstraat", "207, Van der Palmkade"],
+  "Rotterdam" => ["12, Keilezijweg", "131, Bunschotenweg", "60A, Westerbeekstraat", "39, Charlie Shaverslaan", "31, Jean Sibeliusstraat", "6, Mesdaglaan", "48A, Burgemeester Hoffmanplein", "Galvanaistraat", "364, Plaszoom", "125, Tinbergenlaan"],
+  "Utrecht" => ["Torenburg 6", "Prinses Beatrixstraat 8", "Mozartlaan 29", "Orchideeënstraat 11", "Scharenslijperspad 25"],
+  "The Hague" => ["Van Marumstraat 14", "Vijzelstraat 2", "Johannes Vijghstraat 28", "Rijnstraat 15", "Berkter Hei 2"]
+}
 
-  start_date = Date.today
-  duration = rand(3..7)
-  end_date = start_date + duration
-  booking = Booking.new(start_date: start_date, end_date: end_date, user: eirene)
-  booking.flat = flat
-  booking.save!
+nl_addresses.each do |city, array|
+  array.each do |street|
+    file = URI.open("https://source.unsplash.com/random/600x400/?#{city}")
+    price = rand(39..109)
+    description = "- Free parking on site\n
+                  - Indoor swimming pool\n
+                  - Not suitable for youth groups\n
+                  - Optional: Bedlinen incl towels\n\n
+
+                  No pets allowed\n
+                  Minimum age: 18 years"
+    flat = Flat.new(address: street, city: city, flat_location: "NL", price: price, user: admin, description: description)
+    flat.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+    flat.save!
+
+    start_date = Date.today
+    duration = rand(3..7)
+    end_date = start_date + duration
+    booking = Booking.new(start_date: start_date, end_date: end_date, user: eirene)
+    booking.flat = flat
+    booking.save!
+  end
 end
 
-puts "50 flats created with 1 booking for each flat!"
+puts "30 flats in NL created with 1 booking for each flat!"
+# puts "---\n"
+# puts "Creating 10 flats around the world..."
+
+# 10.times do
+#   address = Faker::Address
+#   street = address.full_address
+#   city = address.city
+#   country = address.country
+#   # file = URI.open("https://source.unsplash.com/random/600x400/?#{city}")
+#   price = rand(39..109)
+#   flat = Flat.new(address: street, city: city, flat_location: country, price: price, user: admin)
+#   # flat.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+#   flat.save!
+# end
+
+# puts "10 flats created!"
