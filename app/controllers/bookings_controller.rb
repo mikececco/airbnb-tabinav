935eae2d68140b2e62fe33c32d4a011ac50bac9c
Mike Cecconello <mikececco2000@gmail.com>
2023-03-02 13:15:56 +0100

    Allowing to create and show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,21 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show]
+
   def index
     @bookings = Booking.where(params[:user_id] == current_user)
   end
 
   # show: will display the details of a specific booking, such as the price and location
   def show
-    @booking = Booking.find(params[:id])
     @flat = @booking.flat
+    authorize(@booking)
   end
 
   # new: will display a form to create a new booking
   def new
     @booking = Booking.new
     @flat = Flat.find(params[:flat_id])
+    authorize(@booking)
   end
 
   def create
@@ -20,6 +23,8 @@ class BookingsController < ApplicationController
     @flat = Flat.find(params[:flat_id])
     @booking.flat = @flat
     @booking.user = current_user
+    authorize(@booking)
+
 
     if @booking.save
       redirect_to booking_path(@booking)
@@ -32,5 +37,9 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
